72fcabcf66ad02f65afcc09be388e519f72027d5
Vaclav <kremen.vaclav@mayo.edu>
2019-09-19 15:14:20 -0500

    Update Contents.m

--- Contents.m
+++ Contents.m
@@ -1,7 +1,7 @@
 % PRODUCTION VERSION OF CODE
 %
 % Files
-%   Assess_the_sleep   - Semi-automated assesment of sleep based on feature analysis of EEG
+%   Assess_the_sleep   - Semi-automated assesment of sleep based on feature analysis of EEG. Runs and displays attributes of the time-domain data and launches UI for expert to classify 30-second epochs of the data into AASM2012 sleep classes.
 %   ExtractUWdata      - Prepares the data from particular project with University
 %   feature_extraction - Extracts defined features from EEG or time-domain data.
 %   UW_sleep_wrapper   - Wrapper script that jumps through the number of defined directories Extract University of Wisconsin data by ExtractUWdata script to format that can be taken by Assess_the_sleep function to semi-automatically score the sleep from EEG.
