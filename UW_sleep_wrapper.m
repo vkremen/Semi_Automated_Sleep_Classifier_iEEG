bda3dbb1c2817238894d8cf86084008cc258201b
Vaclav <kremen.vaclav@mayo.edu>
2019-09-19 23:49:20 -0500

    Corrected reading features from file
    Corrected reading features from file and linking subject_id's as global from Matlab Workspace.

--- UW_sleep_wrapper.m
+++ UW_sleep_wrapper.m
@@ -1,16 +1,19 @@
 %% UW_SLEEP_WRAPPER Wrapper script that jumps through the number of defined directories Extract University of Wisconsin data by ExtractUWdata script to format that can be taken by Assess_the_sleep function to semi-automatically score the sleep from EEG.
 %
-% SYNOPSIS: UW_sleep_wrapper(subject_id)
+% SYNOPSIS: UW_sleep_wrapper()
 %
 % INPUT List of directory names in format: subject_id = {'409_041_0001'
-%       '409_041_0002' '369_074_0000' '384_038_0000'};
+%       '409_041_0002' '369_074_0000' '384_038_0000'}; needs to be in
+%       Matlab Workspace as global variable. Variable z needs to be in
+%       Matlab Workspace as global variable too and can be set either as
+%       empty (z=[];).
 %       It requires subfolders with the same names as defined above containing data file 
 %       with subject_id.mat name with raw data.
 % 
 % The script automates extraction of the data and semi-automated sleep
 %       scoring of data originally prepared for a particular projects with 
 %       University of Wisconsin at Madison. It sequentially runs data extraction from 
-%       UW format for each subject and then runs Assess_the_sleep for each subject. 
+%       UW format for each subject defined in subject_id and then runs Assess_the_sleep for each subject. 
 %       The data extraction routines can be addopted for any data format and 
 %       produce data structure that can be then taken by Assess_the_sleep.m function.
 %
@@ -38,16 +41,16 @@
 
 
 %%
-function UW_sleep_wrapper(subject_id)
-
+function UW_sleep_wrapper()
+global subject_id z;
 %% run to extract data from UW structure to one matrix and save
 % it needs to have electrodes data in Matlab Workspace in folder subject_id/subject_id.mat
-    for i = 1 : length(subject_id)
-        ExtractUWdata(subject_id{i}); % prepare data for sleep scoring
+    for z = 1 : length(subject_id)
+        ExtractUWdata(subject_id{z}); % prepare data for sleep scoring
     end
     
-    for i = 1 : length(subject_id)
-        Assess_the_sleep(subject_id{i}); % do a sleep scoring
+    for z = 1 : length(subject_id)
+        Assess_the_sleep(); % do a sleep scoring
     end
     
    
